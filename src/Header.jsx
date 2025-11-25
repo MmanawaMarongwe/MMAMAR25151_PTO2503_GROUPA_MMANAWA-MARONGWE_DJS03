15dcaf3f3c4e669d2f5b3f97a110338f18a20b6e
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-11-25 08:32:36 +0200

    Add podcast header component and import into the App component

---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+import podcastLogo from "./assets/podcast-logo.png";
+import searchIcon from "./assets/search-icon.png";
+import userImg from "./assets/user-profile.png";
+import "./App.css";
+
+export default function Header() {
+  return (
+    <header class="app-header">
+      <div class="app-identity">
+        <img
+          src={podcastLogo}
+          alt="A picture of the Podcast logo"
+          class="app-logo"
+        />
+        <h1 class="app-name">CodeCast</h1>
+      </div>
+
+      <div class="user-section">
+        <img
+          src={searchIcon}
+          alt="A picture of a search icon"
+          class="search-icon"
+        />
+        <img src={userImg} alt="A user profile picture" class="profile-pic" />
+      </div>
+    </header>
+  );
+}
