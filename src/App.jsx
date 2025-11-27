d1cb5903268282fd006d43cb28c8bda369a5a8ab
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-11-27 18:13:52 +0200

    add loading and error state handling in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchPodcasts } from "./api/fetchPodcasts";
 import Header from "./components/Header";
 import Filters from "./components/Filters";
 import "./App.css";
 
-function App() {
+export default function App() {
+  const [podcasts, setPodcasts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchPodcasts(setError, setLoading, setPodcasts);
+  }, []);
+
   return (
     <>
       <Header />
       <Filters />
+      <main>
+        {loading && <p>Loading Podcasts</p>}
+
+        {error && <p>Error occurred while fetching podcasts: {error}</p>}
+
+        {!loading && !error && <Header />}
+      </main>
     </>
   );
 }
-
-export default App;
