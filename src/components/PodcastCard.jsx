ef821f553caf99375110f02732bae568cc90a4d4
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-11-28 07:21:16 +0200

    Update the destructuring and add a Placeholder for genres

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -1,15 +1,15 @@
-export default function PodcastCard({
-  podcast: { title, image, seasons, updated },
-}) {
+export default function PodcastCard({ title, image, seasons, updated }) {
   return (
-    <div class="card">
+    <div className="card">
       <img src={image} className="pod-cover" alt="" />
       <div>
         <h4 id="title">{title}</h4>
         <p id="seasons" className="seasons-text">
           {seasons}
         </p>
-        <div id="genres" className="genre-tags"></div>
+        <div id="genres" className="genre-tags">
+          Placeholder
+        </div>
         <p id="updated" className="text-muted">
           {updated}
         </p>
